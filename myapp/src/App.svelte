44351c3995565f6b9039cccb349e5ac1f8ebd3af
CamilleDelmas <kamilledelmas@gmail.com>
2026-01-12 12:13:27 +0100

    initialisation du projet svelte. Ajout d'un dossier doc pour avoir sous la main le  moodboard et les wireframes.

---
+++ myapp/src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+
+
+</script>
+
+<nav>
+  
+</nav>
+<main>
+  
+</main>
+
+<style>
+
+/*Large mobile*/
+@media (min-width: 576px) { /* ... */ }
+
+/* Tablettes */
+@media (min-width: 768px) { /* ... */ }
+
+/* Desktop */
+@media (min-width: 992px) { /* ... */ }
+
+/* Desktop HD */
+@media (min-width: 1200px) { /* ... */ }
+
+/* Desktop Wide */
+@media (min-width: 1400px) { /* ... */ }
+
+</style>
