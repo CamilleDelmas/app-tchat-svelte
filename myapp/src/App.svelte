65f85bf08f08864bc0f790555d19b20c78abc9a1
CamilleDelmas <kamilledelmas@gmail.com>
2026-01-13 16:30:42 +0100

    creations de deux composants pour le menu (un pour le mobile et un pour  le desktop) puis affichage avec condition selon taille du viewport

--- myapp/src/App.svelte
+++ myapp/src/App.svelte
@@ -1,14 +1,13 @@
 <script>
 
   // ========== IMPORTS ==========================================
-  import { MailMinus, Menu } from "@lucide/svelte";
-  import { User } from "@lucide/svelte";
-  import { Plus } from "@lucide/svelte";
-  import { MessageSquareText } from "@lucide/svelte";
-  import { Trash2 } from "@lucide/svelte";
-  import Markdown from "svelte-exmarkdown";
 
+  import Markdown from "svelte-exmarkdown";
+  import MobileNav from "./lib/MobileNav.svelte";
+  import DesktopNav from "./lib/DesktopNav.svelte";
+  
   // let active = false;
+
 
   // ========== VARIABLES ========================================
   // Variable pour stocker la valeur de mon textarea
@@ -19,6 +18,9 @@
   let robotTalk = $state({});
   // Tableau qui stock tous les messages reçus de l'IA
   let savedRobotTalk = $state([]);
+
+let innerWidth = $state(0); 
+
 
   // ========== TOKEN ============================================
   let userToken = $state("");
@@ -84,55 +86,14 @@
 
   // localStorage.clear()
 </script>
+<svelte:window bind:innerWidth />
 
 <div class="container">
-  <header>
-    <section class="head-desktop">
-      <h1>O'Chat</h1>
-    </section>
-    <section class="head-mobile">
-      <!-- TODO: bug sur la version desktop pour l'affichage de la classe active. A revoir !!! -->
-      <!-- <button
-        id="burger-button"
-        class="burger"
-        onclick={() => (active = !active)}
-      > -->
-      <button id="burger-button" class="burger">
-        <Menu size="42px" />
-      </button>
-      <h1>O'Chat</h1>
-      <User size="42px" />
-    </section>
-    <!-- <nav class="menu" class:active> -->
-    <nav class="menu">
-      <div class="menu__add separate">
-        <button type="submit"><Plus size={42} /></button>
-        <input id="new-talk" type="text" placeholder="Nouvelle discussion" />
-      </div>
-      <div class="menu__add">
-        <MessageSquareText size={38} strokeWidth={1.5} />
-        <p>Discussions récentes</p>
-      </div>
-      <div class="menu__talk">
-        <div class="talk">
-          <p>Javascript</p>
-          <button>
-            <Trash2 size={24} strokeWidth={1.5} />
-          </button>
-        </div>
-        <div class="talk">
-          <p>Javascript</p>
-          <button>
-            <Trash2 size={24} strokeWidth={1.5} />
-          </button>
-        </div>
-      </div>
-    </nav>
-    <section class="head-user">
-      <User size="42px" />
-      <p>Nom d'utilisateur</p>
-    </section>
-  </header>
+  {#if innerWidth < 1200}
+  <MobileNav />
+  {:else}
+  <DesktopNav />
+  {/if}
   {#if myToken !== null}
     <main>
       <div class="robot-talk">
@@ -142,8 +103,6 @@
           ici !
         </p>
       </div>
-      <!-- TODO: réparer l'affichage -->
-      <!-- Problème d'affichage : les messages de l'utilisateur restent en haut, suivis par ceux de l'IA. Comportement attendu : un message de l'utilisateur puis un message de l'IA, etc. -->
       {#if savedTalk.length > 0}
         {#each savedTalk as talk}
           {#if talk.role === "user"}
@@ -214,95 +173,6 @@
       }
       &:focus {
         background-color: var(--hilight-color);
-      }
-    }
-  }
-
-  /* Header */
-
-  header {
-    position: sticky;
-    top: 0;
-    width: 100%;
-    padding: 1rem;
-    background-color: var(--dark-color);
-  }
-  .head-desktop {
-    display: none;
-  }
-  .head-mobile {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-  }
-  .head-user {
-    display: none;
-  }
-  .burger {
-    background-color: var(--dark-color);
-    color: var(--neutral-color);
-  }
-  .active {
-    position: absolute;
-    top: -300px;
-  }
-  .menu {
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-    padding: 1rem;
-    .menu__add {
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      gap: 1rem;
-      input {
-        padding: 0.2rem;
-        width: 80%;
-        border: none;
-        background-color: var(--dark-color);
-        outline: none;
-        color: var(--neutral-color);
-        &::placeholder {
-          color: var(--hilight-color);
-        }
-        &:focus {
-          border-right: 1px solid var(--neutral-color);
-        }
-      }
-    }
-
-    .separate {
-      position: relative;
-      margin-block-end: 2rem;
-    }
-    .separate::after {
-      position: absolute;
-      bottom: -20px;
-      content: "";
-      height: 1px;
-      width: 100%;
-      background-color: var(--main-color);
-    }
-    .menu__talk {
-      display: flex;
-      flex-direction: column;
-      gap: 1rem;
-    }
-    .talk {
-      display: flex;
-      flex-direction: row;
-      justify-content: flex-end;
-      align-items: center;
-      gap: 1rem;
-      p {
-        color: var(--neutral-color-color);
-        text-align: left;
-        background-color: var(--main-color);
-        min-width: 10rem;
-        padding: 0.3rem 1rem;
-        border-radius: 1rem;
       }
     }
   }
@@ -452,52 +322,15 @@
 
   } */
 
-  /* Desktop HD */
+    /* Main */
   @media (min-width: 1200px) {
     .container {
       width: 100%;
       height: 100%;
       display: flex;
       flex-direction: row;
+      border: 4px solid green;
     }
-
-    /* Header */
-
-    header {
-      top: 30%;
-      left: 0;
-      transform: translateY(-30%);
-      width: 350px;
-      height: 500px;
-      padding: 1rem;
-      background-color: var(--dark-color);
-      border-radius: 0 1rem 1rem 0;
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-    }
-
-    .head-desktop {
-      h1 {
-        font-size: 52px;
-      }
-      display: block;
-      position: absolute;
-      top: -50px;
-    }
-    .head-mobile {
-      display: none;
-    }
-    .head-user {
-      margin: 1rem;
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      gap: 1rem;
-    }
-
-    /* Main */
-
     main {
       width: 60%;
       margin: 2rem auto;
@@ -523,7 +356,6 @@
       min-height: 2rem;
     }
   }
-
   /* Desktop Wide */
   @media (min-width: 1400px) {
     /* ... */
